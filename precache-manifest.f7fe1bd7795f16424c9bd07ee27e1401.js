832204039afe5453e629d0d6ba3dd9054b5cce27
Pritam Ghosh <meghoshpritam@gmail.com>
2020-06-19 17:46:25 +0530

    Updates

---
+++ precache-manifest.f7fe1bd7795f16424c9bd07ee27e1401.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a50ef1a566f782a4f8344a3ca989bbc",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "575c442e906314fbf2b3",
+    "url": "/gallery/static/css/main.e05bd2ff.chunk.css"
+  },
+  {
+    "revision": "58639ce5c3c0e902a08a",
+    "url": "/gallery/static/js/2.ee06216c.chunk.js"
+  },
+  {
+    "revision": "2bddf808aa595e6c6f0c61a3e69ad57d",
+    "url": "/gallery/static/js/2.ee06216c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575c442e906314fbf2b3",
+    "url": "/gallery/static/js/main.45d9f578.chunk.js"
+  },
+  {
+    "revision": "7dd180262069cbb141c9",
+    "url": "/gallery/static/js/runtime-main.27649e68.js"
+  },
+  {
+    "revision": "32affcb5dd81989ce209b59c51bc9d61",
+    "url": "/gallery/static/media/1.32affcb5.jpg"
+  },
+  {
+    "revision": "45b7937dc6e1c751e2fa9579c0fa68c0",
+    "url": "/gallery/static/media/2.45b7937d.jpg"
+  },
+  {
+    "revision": "65c0461c19087ee453b406b00af80b49",
+    "url": "/gallery/static/media/3.65c0461c.jpg"
+  },
+  {
+    "revision": "123c487d360228d7c031dd9719aa70b1",
+    "url": "/gallery/static/media/4.123c487d.jpg"
+  }
+]);
